a40c56ecd15ff763022504f61dd40c3e98af772c
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-07-31 19:46:23 +0200

    Create config-client project to consume config-server

---
+++ config-client/src/test/java/tech/mahmoudfawzy/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.mahmoudfawzy.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
