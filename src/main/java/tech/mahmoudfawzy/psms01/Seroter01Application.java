7504af3ce110da048f155584a451f992f1288159
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-07-31 18:19:30 +0200

    Initial commit.

---
+++ src/main/java/tech/mahmoudfawzy/psms01/Seroter01Application.java
@@ -0,0 +1,13 @@
+package tech.mahmoudfawzy.psms01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seroter01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Seroter01Application.class, args);
+    }
+
+}
---
+++ src/test/java/tech/mahmoudfawzy/psms01/Seroter01ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.mahmoudfawzy.psms01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seroter01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
