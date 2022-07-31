a4af28f4442c5e809c34bc66f33ca8030b4155d2
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-07-31 22:36:18 +0200

    Enable refreshing properties while client is up by POST /actuator/refresh

--- config-client/src/main/java/tech/mahmoudfawzy/configclient/RateController.java
+++ config-client/src/main/java/tech/mahmoudfawzy/configclient/RateController.java
@@ -1,12 +1,14 @@
 package tech.mahmoudfawzy.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RefreshScope
 @RequestMapping("/rates")
 public class RateController {
 
