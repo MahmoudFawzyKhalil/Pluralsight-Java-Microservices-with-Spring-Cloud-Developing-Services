0389dcb5e7508bb0cbd287fe92722b803e1304f9
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-07-31 21:02:15 +0200

    Add key to support /encrypt endpoint and disable CSRF and endpoint http security

--- config-server/src/main/java/tech/mahmoudfawzy/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/tech/mahmoudfawzy/configserver/ConfigServerApplication.java
@@ -3,6 +3,9 @@ package tech.mahmoudfawzy.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableConfigServer
 @SpringBootApplication
@@ -12,4 +15,15 @@ public class ConfigServerApplication {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
 
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.authorizeRequests()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .csrf()
+                .disable()
+                .build();
+    }
 }
